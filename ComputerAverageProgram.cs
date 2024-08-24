f64e536811dcfa73375501c70b661c11c6194cd9
Acedia <tamayoclydejohn2005@gmail.com>
2024-08-24 22:27:03 +0800

    main

---
+++ ComputerAverageProgram.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter 5 Grades separated by New lines: ");
+double grades0 = Convert.ToDouble(Console.ReadLine());
+double grades1 = Convert.ToDouble(Console.ReadLine());
+double grades2 = Convert.ToDouble(Console.ReadLine());
+double grades3 = Convert.ToDouble(Console.ReadLine());
+double grades4 = Convert.ToDouble(Console.ReadLine());
+
+double sum = grades0 + grades1 + grades2 + grades3 + grades4;
+double average = sum/5;
+Console.WriteLine("The average grade is: " + average + " rounded off to " + Convert.ToInt32(average));
--- obj/Debug/net8.0/ComputerAverageApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ComputerAverageApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComputerAverageApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a71342159ba97c65e1d6366c3f03acdc467c104")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComputerAverageApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComputerAverageApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
